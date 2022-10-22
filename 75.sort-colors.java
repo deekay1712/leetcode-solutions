f56350a6f3e000e84c7bf01133ea6d7fd09bd58e
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2022-10-22 18:51:47 +0530

    :pencil2: sort-colors

---
+++ 75.sort-colors.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=75 lang=java
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+class Solution {
+    public void sortColors(int[] nums) {
+        int low, high, mid;
+        low = 0;
+        high = nums.length - 1;
+        mid = 0;
+
+        while(mid <= high){
+            switch(nums[mid]){
+                case 0:
+                    swap(nums, low, mid);
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums, mid, high);
+                    high--;
+                    break;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+// @lc code=end
+
