e5537337ccae9f1ef972dd7552be34978617bda9
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2022-10-22 18:50:28 +0530

    :pencil2: two-sum

---
+++ 1.two-sum.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        // since only one solution, we don't need to return anything
+        return new int[]{};
+    }
+}
+// @lc code=end
+
