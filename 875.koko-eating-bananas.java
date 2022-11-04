74decd618832ec75b200ff1c340676f5c096e339
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2022-11-04 23:32:29 +0530

    :pencil2: koko-eating-bananas

---
+++ 875.koko-eating-bananas.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=875 lang=java
+ *
+ * [875] Koko Eating Bananas
+ */
+
+// @lc code=start
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = 1000000000;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (canFinish(piles, h, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    private boolean canFinish(int[] piles, int h, int k) {
+        int time = 0;
+        for (int pile : piles) {
+            time += (pile - 1) / k + 1;
+        }
+        return time <= h;
+    }
+}
+// @lc code=end
+
