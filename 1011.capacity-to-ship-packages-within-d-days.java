3f844ec7c83f23d935bf7905d138c2501c1f61bb
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2022-12-27 08:14:15 +0530

    :pencil2: capacity-to-ship-packages-within-d-days

---
+++ 1011.capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int left = 0, right = 0;
+        for(int w : weights){
+            left = Math.max(left, w);
+            right += w;
+        }
+        while(left < right){
+            int mid = (left + right) / 2;
+            int need = 1, cur = 0;
+            for(int w : weights){
+                if(cur + w > mid){
+                    need++;
+                    cur = 0;
+                }
+                cur += w;
+            }
+            if(need > days){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+
+    }
+}
