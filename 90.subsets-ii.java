3f6c953245b1d1859bba03ca4985f499299be8c3
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2023-06-10 18:50:25 +0530

    subsets-ii.java

---
+++ 90.subsets-ii.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=90 lang=java
+ *
+ * [90] Subsets II
+ */
+
+// @lc code=start
+
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        permute(nums, 0, res, new ArrayList<Integer>());
+        return res;
+    }
+
+    public void permute(int[] nums, int start, List<List<Integer>> res, List<Integer> tempList) {
+        res.add(new ArrayList<>(tempList));
+
+        for(int i=start; i<nums.length; i++){
+            if(i>start && nums[i]==nums[i-1])
+                continue;
+
+            tempList.add(nums[i]);
+            permute(nums, i+1, res, tempList);
+            tempList.remove(tempList.size()-1);
+        }
+    }
+}
+// @lc code=end
+
